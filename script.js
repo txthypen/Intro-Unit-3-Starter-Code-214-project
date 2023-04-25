e8f66cac66004a18b4e8cd0bbe0a8240c9797c6a
KameaPettway <17630350-KameaPettway@users.noreply.replit.com>
2023-04-25 13:24:08 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,28 @@
+/* Declare variables below to save the different sections (divs) of your story*/
+let gameButton = document.querySelector(".option-one")
+let moveButton = document.querySelector(".option-two")
+let optiononeScreen= document.querySelector(".option-one-screen")
+
+
+gameButton.onclick=function(){
+   optiononeScreen.style.display="block"
+}
+
+
+
+
+/* When you're ready to make event handlers, uncomment the code below. Then fill in the blanks with the correct variables!
+INSERT_VARIABLE.onclick=function(){
+
+};
+
+INSERT_VARIABLE.onclick=function(){
+
+};
+
+
+INSERT_VARIABLE.onclick=function(){
+
+};
+
+*/
